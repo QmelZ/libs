a5c9d3e033ddbe929febb3fc2bba875a193f3cad
QmelZ <qmelzthevoid@gmail.com>
2021-03-23 02:49:09 +0300

    fix events

--- libs/events.js
+++ libs/events.js
@@ -5,30 +5,33 @@ let events = {
         if(!event || !run) return;
         if(typeof event !== "string") return;
         if(typeof run !== "function") return;
-        
         if(!eventlist[event]) eventlist[event] = [];
+        
         eventlist[event].push(run);
     },
-    run: events.on,
     
     remove(event, run){
         if(!event || !run) return;
         if(typeof event !== "string") return;
         if(typeof run !== "function") return;
-        
         if(!eventlist[event]) return;
+        
         eventlist[event] = eventlist[event].filter(e => e !== run);
     },
     
     clear(event){
-        if(!event){
-            eventlist = {};
-        }else{
-            eventlist[event] = [];
-        }
+        if(!event) return;
+        if(typeof event !== "string") return;
+        if(!eventlist[event]) return;
+        
+        eventlist[event] = [];
     },
     
     fire(event){
+        if(!event) return;
+        if(typeof event !== "string") return;
+        if(!eventlist[event]) return;
+        
         eventlist[event].forEach(e => e());
     }
 };
